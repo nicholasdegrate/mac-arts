bf70fc168dded67cf700c85b6c3217ad75f12fa6
Dan Leaver <dleaver@gmail.com>
2020-11-30 21:08:58 -0700

    education page rough css
    dunno about the year tabs...

--- src/pages/education/Education.js
+++ src/pages/education/Education.js
@@ -1,192 +1,164 @@
-
+/** @jsxImportSource @emotion/react */
 import { jsx, css } from '@emotion/react';
-import './educationStyles.css'
-import './styles.css'
+import FilterBar from '../../components/navbar/FilterBar';
+import Year from '../../components/project/Year';
+
 const Education = () => (
-  <div>
-    <div className="title_bar">
-      <div className="stoyan_title">
+  <>
+    <div style={{position: "fixed", top: "4rem", width: "100%"}}>
+      <FilterBar selected={"Education"} />
+    </div>
+    <div style={{height: "4rem"}} />
+    <div style={{padding: "1em"}} />
+    {/* <div 
+      css={css`
+        font-size: 24px;
+        text-align: center;
+        color: white;
+        background: #880000;
+        padding: 1rem;
+        @media (min-width: 768px) {
+          padding: 2rem;
+          font-size: 36px;
+        }
+      `}
+    >
       The Stoyan Christowe Endowment Fund   
-      </div>
-    </div>
+    </div> */}
 
-        <div className="right_column_content">
-        <div className="universities"><img src="http://macedonianarts.org/images/universities.png" /></div>
-        
-        <div className="content_box3">
-          <div className="jfk_quote" style={{clear: "both"}}>
-                    <i>"Let us think of education as the means of developing our greatest abilities because in each of us there is a private hope and dream which fulfilled, can be translated into benefit for everyone"</i><br/>
-                    <span style={{float: "right"}}>
-                    – John F. Kennedy
-                    </span>
-                </div>
-                <br/>
-          <p>To honor the extraordinary life of Stoyan Christowe, and to uphold and keep alive his literary work, in 2010 the Macedonian Arts Council established The <span style={{color:"#880000"}}><i>STOYAN CHRISTOWE SCHOLARSHIP FUND</i></span>.  The first three graduates granted a scholarship earned their BA degrees in classical studies and literature, in  May 2014.  The total contribution by the Arts Council was $12,000. To ensure ongoing, permanent support of students in need of financial aid, the Scholarship will morph into an Endowment Fund.</p>
-    <p>The Macedonian Arts Council is in discussions with several institutions that played a major role in the shaping of  Stoyan Christowe’s  writing and political careers,  for the purpose of establishing a permanent endowment fund through which  each year one  economically disadvantaged  high school graduate in Macedonia will be selected to attend college at the <a href="http://www.ukim.edu.mk/" target="new"><b>Sts. Cyril and Methodius University</b></a> in the capitol, Skopje.  The field of studies will be limited to the two subjects that Stoyan Christowe was passionate about and excelled in:  Literature and/or political science. The endowment fund will support four students,  each in a different  academic year  and it will be managed, in perpetuity, by the US based education institution.</p>
+    <div 
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 1rem;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <div>
+        <Year width={"325px"} year={"The Stoyan Christowe Endowment Fund"} />
 
-
-    <a href="#"><div id="downloadFont" className="download_button">Download Application&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Political Science&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Literature</div></a>
-        
-    </div>
-
-    <table id="maintable">
-    <tr style={{padding: ".625em"}}>
-    <td>
-      <table>
-        <tr>
-          <td><img className="profile_pic" src="http://macedonianarts.org/images/stoyan.jpg"/></td>
-          <td><p><b>Stoyan Christowe<br/>
-          1897-1995<br/>
-          Statesman, Journalist, Author</b></p></td>
-        </tr>
-        <tr>
-          <td>
-            <p className="content_box1" style={{marginLeft: "-1%"}}>Born in Ottoman Macedonia in 1898, Stoyan Christowe immigrated to the United States in 1911, with a burning desire to be an American’s American. He came of age on the Great Plains and went on to be a well-known journalist, critic and lover of the English language. In all, he wrote six books, numerous short stories and articles. He ended his career as a Vermont State Representative and then a Senator. Stoyan Christowe remained passionate about his adopted country throughout his life and is remembered as a staunch defender of the democratic way. His is a quintessential American immigrant story that will be remembered through an endowment fund in support of students of literature and political science, in his native Macedonia. </p>
-          </td>
-        </tr>
-      </table>
-    </td>
-
-    <td style={{padding: ".625em"}}><div className="quotes" style={{marginRight: "-2.5%"}}>
-    <div className="name">From <i>The Eagle and the Stork</i></div>
-    <i>My mind was astir like a beehive ready to swarm….…thoughts and ideas, bits of knowledge facts, rays of imagination hovered and darted in my mind…. If only I could absorb in my own mind, as much knowledge, knowledge of politika, of diplomatia, and the language, the English language…</i><br/><br/>
-
-    <i>With my growing knowledge of the language America itself grew before my vision, etched itself out more clearly, and captivated my soul more enduringly.</i><br/><br/>
-
-    <i>From the hallways of my memory haiduks and comitadjis stepped down to make room for  Paul Bunyan,  Buffalo Bill, Jesse James General Custer. The new was so much bigger than the old. America was bigger than what she displaced.  The vast plain, even, was not vast enough to contain my desires. There came, then, the hunger and the thirst for an America that I could not bind with a band of steel, or nail down with a spike.</i><br/><br/>
-
-    <i>There began to seep through my being, like a strong potion, a vitalizing American serum. My young body became possessed of a passionate yearning to be absorbed by America. I longed, like a youth in love, to lay my head on the breast of America.</i>
-    </div>
-    </td>
-    </tr>
-    </table>
-
-    <div id="responsivetable" className="row">
-    <div className="column">
-    <table>
-    <tr>
-      <td>
-        <img className="profile_pic" src="http://macedonianarts.org/images/stoyan.jpg" style={{width: "75%"}} />
-      </td>
-
-      <td>
-      <p><b>Stoyan Christowe<br/>
-        1897-1995<br/>
-        Statesman, Journalist, Author</b></p>
-      </td>
-    </tr>
-    </table>
-
-    <table>
-    <tr>
-      <td>
-        <p style={{textAlign: "justify"}}>Born in Ottoman Macedonia in 1898, Stoyan Christowe immigrated to the United States in 1911, with a burning desire to be an American’s American. He came of age on the Great Plains and went on to be a well-known journalist, critic and lover of the English language. In all, he wrote six books, numerous short stories and articles. He ended his career as a Vermont State Representative and then a Senator. Stoyan Christowe remained passionate about his adopted country throughout his life and is remembered as a staunch defender of the democratic way. His is a quintessential American immigrant story that will be remembered through an endowment fund in support of students of literature and political science, in his native Macedonia. </p>
-    </td>
-    </tr>
-    </table>
-    </div>
-
-    <div className=" column quotes" style={{marginRight: "-2.5%"}}>
-      <div className="name">From <i>The Eagle and the Stork</i></div>
-      <i>My mind was astir like a beehive ready to swarm….…thoughts and ideas, bits of knowledge facts, rays of imagination hovered and darted in my mind…. If only I could absorb in my own mind, as much knowledge, knowledge of politika, of diplomatia, and the language, the English language…</i><br/><br/>
-
-      <i>With my growing knowledge of the language America itself grew before my vision, etched itself out more clearly, and captivated my soul more enduringly.</i><br/><br/>
-
-      <i>From the hallways of my memory haiduks and comitadjis stepped down to make room for  Paul Bunyan,  Buffalo Bill, Jesse James General Custer. The new was so much bigger than the old. America was bigger than what she displaced.  The vast plain, even, was not vast enough to contain my desires. There came, then, the hunger and the thirst for an America that I could not bind with a band of steel, or nail down with a spike.</i><br/><br/>
-      
-      <i>There began to seep through my being, like a strong potion, a vitalizing American serum. My young body became possessed of a passionate yearning to be absorbed by America. I longed, like a youth in love, to lay my head on the breast of America.</i>
-      </div>
-    </div>
-
-    <div id="responsivetable2" className="row">
-    <table>
-    <tr>
-    <td>
-    <div className="column">
-      <table>
-      <tr>
-        <td>
-          <img className="profile_pic" src="http://macedonianarts.org/images/stoyan.jpg" style={{width: "100%"}} />
-        </td>
-
-        <td>
-        <p><b>Stoyan ChristoweHERHEHRE<br/>
-          1897-1995<br/>
-          Statesman, Journalist, Author</b></p>
-        </td>
-      </tr>
-    </table>
-
-    <table>
-      <tr>
-        <td>
-          <p style={{textAlign: "justify"}}>Born in Ottoman Macedonia in 1898, Stoyan Christowe immigrated to the United States in 1911, with a burning desire to be an American’s American. He came of age on the Great Plains and went on to be a well-known journalist, critic and lover of the English language. In all, he wrote six books, numerous short stories and articles. He ended his career as a Vermont State Representative and then a Senator. Stoyan Christowe remained passionate about his adopted country throughout his life and is remembered as a staunch defender of the democratic way. His is a quintessential American immigrant story that will be remembered through an endowment fund in support of students of literature and political science, in his native Macedonia. </p>
-      </td>
-    </tr>
-    </table>
-    </div>
-    </td>
-    </tr>
-
-
-    <tr>
-      <td>
-      <div className=" column quotes" style={{marginRight: "-2.5%"}}>
-        <div className="name">From <i>The Eagle and the Stork</i></div>
-        <i>My mind was astir like a beehive ready to swarm….…thoughts and ideas, bits of knowledge facts, rays of imagination hovered and darted in my mind…. If only I could absorb in my own mind, as much knowledge, knowledge of politika, of diplomatia, and the language, the English language…</i><br/><br/>
-
-        <i>With my growing knowledge of the language America itself grew before my vision, etched itself out more clearly, and captivated my soul more enduringly.</i><br/><br/>
-
-        <i>From the hallways of my memory haiduks and comitadjis stepped down to make room for  Paul Bunyan,  Buffalo Bill, Jesse James General Custer. The new was so much bigger than the old. America was bigger than what she displaced.  The vast plain, even, was not vast enough to contain my desires. There came, then, the hunger and the thirst for an America that I could not bind with a band of steel, or nail down with a spike.</i><br/><br/>
-        
-        <i>There began to seep through my being, like a strong potion, a vitalizing American serum. My young body became possessed of a passionate yearning to be absorbed by America. I longed, like a youth in love, to lay my head on the breast of America.</i>
+          <img 
+            alt="scef"
+            height="100%"
+            width="100%"
+            src="http://macedonianarts.org/images/universities.png" 
+          />
         </div>
-        </td>
-        </tr>
-    </table>
-    </div>
+      </div>      
+      <div
+        css={css`
+          background: #E6E5E1;
+          padding: 1rem;
+          margin-bottom: 1rem;
+          color: #880000;
+          max-width: calc(1024px - 2rem);
+          margin-top: 1rem;
+        `}
+      >
+        "Let us think of education as the means of developing our greatest abilities because in each of us there is a private hope and dream which fulfilled, can be translated into benefit for everyone"
+        <span 
+          css={css`
+            float: right;
+            padding: 1rem;
+          `}
+        >
+          – John F. Kennedy
+        </span>
+      </div>
+      <div 
+        css={css`
+          margin-top: -1rem;
+          max-width: 1024px;
+        `}
+      >
+        <p>To honor the extraordinary life of Stoyan Christowe, and to uphold and keep alive his literary work, in 2010 the Macedonian Arts Council established The <span style={{color:"#880000"}}><i>STOYAN CHRISTOWE SCHOLARSHIP FUND</i></span>.  The first three graduates granted a scholarship earned their BA degrees in classical studies and literature, in  May 2014.  The total contribution by the Arts Council was $12,000. To ensure ongoing, permanent support of students in need of financial aid, the Scholarship will morph into an Endowment Fund.</p>
+        <p>The Macedonian Arts Council is in discussions with several institutions that played a major role in the shaping of  Stoyan Christowe’s  writing and political careers,  for the purpose of establishing a permanent endowment fund through which  each year one  economically disadvantaged  high school graduate in Macedonia will be selected to attend college at the <a href="http://www.ukim.edu.mk/" target="new"><b>Sts. Cyril and Methodius University</b></a> in the capitol, Skopje.  The field of studies will be limited to the two subjects that Stoyan Christowe was passionate about and excelled in:  Literature and/or political science. The endowment fund will support four students,  each in a different  academic year  and it will be managed, in perpetuity, by the US based education institution.</p>
+      </div>
+      <div css={css`
+        background: #880000;
+        height: 1rem;
+        width: 100%;
+        padding: 1rem;
+        margin: 1rem;
+      `} >
+      </div>
+      <div //b/w pic and yellow div
+        css={css`
+          display: flex;
+          flex-wrap: wrap;
+          max-width: 1024px;
+          @media (min-width: 665px) {
+            flex-wrap: nowrap;
+          }
+        `}
+      >
+        <div>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+              @media (min-width 768px) {
+                padding: 5rem;
 
-            <div className="endowment_modules">
-      <table style={{padding: "2.5%"}}>
-        <tr>
-          <td>
-          <img src="http://macedonianarts.org/images/girls.jpg" />				
-          </td>
-
-          <td>
-          <img src="http://macedonianarts.org/images/ana.jpg" />						
-          </td>
-        </tr>
-
-        <tr>
-          <td><p>Class of 2014</p></td>
-          <td><p>Ana  Milosheska - <i>Women in the New Тestament</i><br/>
-          (Bachelor of Arts in  South Slavic and <br id="mobilebreak1" /> Macedonian Literature)</p></td>
-        </tr>
-
-        <tr>
-          <td>
-          <img src="http://macedonianarts.org/images/thesis.jpg" />						
-          </td>
-
-          <td>
-          <img src="http://macedonianarts.org/images/Valentina.jpg" />					
-          </td>
-        </tr>
-
-        <tr>
-          <td><p>Vesna Denkina - <i>Orpheus’ Doctrine of the Afterlife</i><br/>
-          (Bachelor of Arts in Classical Philology)</p></td>
-          <td><p>Valentina Gorgievska - <i>Weapons names <br id="mobilebreak2" /> in Mycenaean Greek</i><br/>
-          (Bachelor of Arts in Classical Philology)</p></td>
-        </tr>
-      </table>
+              }
+            `}
+          >
+            <div>
+              <img height="100%" width="100%" alt='stoyan' className="profile_pic" src="http://macedonianarts.org/images/stoyan.jpg"/>
+            </div>
+            <div
+              css={css`
+                display: flex;
+                justify-content: center;
+                width: 100%;
+              `}
+            >
+              <div
+                css={css`
+                  padding: 1rem;
+                  text-align: center;
+                  font-weight: bold;
+                  p { margin: 0.25rem}
+                `}
+              >
+                <p>Stoyan Christowe</p><p>1897-1995</p><p>Statesman, Journalist, Author</p>  
+              </div>
+            </div>
+          </div>
+          <div
+            css={css`
+              padding: 1rem 1rem 1rem 0;
+            `}
+          >
+            Born in Ottoman Macedonia in 1898, Stoyan Christowe immigrated to the United States in 1911, with a burning desire to be an American’s American. He came of age on the Great Plains and went on to be a well-known journalist, critic and lover of the English language. In all, he wrote six books, numerous short stories and articles. He ended his career as a Vermont State Representative and then a Senator. Stoyan Christowe remained passionate about his adopted country throughout his life and is remembered as a staunch defender of the democratic way. His is a quintessential American immigrant story that will be remembered through an endowment fund in support of students of literature and political science, in his native Macedonia.
+          </div>
+        </div>
+        <div 
+          css={css`
+            background-color: #ffd00c;
+            padding: 2rem;
+            flex-basis: 4800px;
+            font-family: 'Cardo', serif;
+            font-size: .9em;
+            line-height: 1.25em;
+            font-weight: 400;
+            color: #494949;
+            text-align: justify;          
+          `}
+        >
+          <div>From <i>The Eagle and the Stork</i></div>
+          <i>My mind was astir like a beehive ready to swarm….…thoughts and ideas, bits of knowledge facts, rays of imagination hovered and darted in my mind…. If only I could absorb in my own mind, as much knowledge, knowledge of politika, of diplomatia, and the language, the English language…</i><br/><br/>        <i>With my growing knowledge of the language America itself grew before my vision, etched itself out more clearly, and captivated my soul more enduringly.</i><br/><br/>        <i>From the hallways of my memory haiduks and comitadjis stepped down to make room for  Paul Bunyan,  Buffalo Bill, Jesse James General Custer. The new was so much bigger than the old. America was bigger than what she displaced.  The vast plain, even, was not vast enough to contain my desires. There came, then, the hunger and the thirst for an America that I could not bind with a band of steel, or nail down with a spike.</i><br/><br/>        <i>There began to seep through my being, like a strong potion, a vitalizing American serum. My young body became possessed of a passionate yearning to be absorbed by America. I longed, like a youth in love, to lay my head on the breast of America.</i>
+        </div>
       </div>
     </div>
-
-
-
-        </div>
+  </>
+  
   
 )
 export default Education
